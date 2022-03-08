3770598a4b45386c0fc3b5b71a68eea36acc2e36
Mike Heavers <mheavers@gmail.com>
2022-03-07 17:28:36 -0800

    some different api methods - all failing due to inability to calculate gas price

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [connected, setConnected] = React.useState(null);
+  const [balance, setBalance] = React.useState(null);
+
+  const ownerAddress = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199";
 
   React.useEffect(() => {
     fetch("/api")
@@ -11,11 +14,37 @@ function App() {
       .then((data) => setData(data.message));
   }, []);
 
+  async function getBalance() {
+    fetch(`/get_balance/${ownerAddress}`)
+      .then((res) => res.json())
+      .then((data) => setBalance(data.balance));
+  }
+
+  async function transferBalance() {
+    const transferAddress = "0x82A820016E3dE2b0c1e259B4aBB275c0E8f8E33F"; //THOMAS
+    fetch(`/transfer_balance/${transferAddress}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
+  async function mintTo() {
+    const transferAddress = "0x82A820016E3dE2b0c1e259B4aBB275c0E8f8E33F"; //THOMAS
+    fetch(`/mint_to/${transferAddress}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
+        <button onClick={getBalance}>getBalance</button>
+        <p>{!balance ? "" : `Balance: ${balance}`}</p>
+        <button onClick={mintTo}>mintTo</button>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,15 +1,102 @@
 const express = require("express");
+const ethers = require("ethers").ethers;
+const ThirdwebSDK = require("@3rdweb/sdk").ThirdwebSDK;
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 const path = require("path");
 const app = express();
 
+// Instantiate thirdrdweb SDK
+
+// This url indicates which chain you want to connect to
+const rpcUrl = "https://rinkeby-light.eth.linkpool.io/"; //rinkeby (ethereum test net)
+// You can switch out this provider with any wallet or provider setup you like.
+//const provider = ethers.Wallet.createRandom(); //from code sample in thirdweb
+const provider = ethers.getDefaultProvider(rpcUrl);
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    // Your wallet private key
+    process.env.PRIVATE_KEY,
+    // RPC URL
+    provider
+  )
+);
+
+// Instantiate Token module
+const token = sdk.getTokenModule("0xc33A4EBa66A3a3Cf84F898f2398966Bf6A2e37a3");
+// Address of the wallet who owns the funds
+const ownerAddress = "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199";
+
+async function getBalance(balanceAddress) {
+  const balance = await token.balanceOf(balanceAddress);
+  return balance;
+}
+
+async function transferBalance(balanceAddress) {
+  const amount = 1; // The number of tokens you want to send
+  // Note that the connected wallet must have approval to transfer the tokens of the fromAddress
+  return await token.transferFrom(ownerAddress, balanceAddress, amount);
+}
+
+async function mintTo(mintAddress) {
+  const amount = 1; // The number of tokens you want to send
+  // Note that the connected wallet must have approval to transfer the tokens of the fromAddress
+  return await token.mintTo(mintAddress, amount);
+}
+
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "connected to server" });
 });
+
+app.get("/get_balance/:userAddress", async (req, res) => {
+  try {
+    console.log("userAddress", req.params.userAddress);
+    const balanceAddress = req.params.userAddress
+      ? req.params.userAddress
+      : ownerAddress;
+
+    const balance = await getBalance(balanceAddress);
+    console.log("balance", balance);
+    const balanceVal =
+      balance.decimals > 0
+        ? balance.displayValue
+        : ethers.utils.formatEther(balance.value);
+    res.json({ balance: balanceVal });
+  } catch (error) {
+    console.log("error", error);
+    res.json({ balance: "error" });
+  }
+});
+
+app.get("/transfer_balance/:userAddress", async (req, res) => {
+  try {
+    const balanceAddress = req.params.userAddress;
+    const balance = await transferBalance(balanceAddress);
+    console.log("balance", balance);
+    res.json({ balance: balanceVal });
+  } catch (error) {
+    console.log("error", error);
+    res.json({ balance: "error" });
+  }
+});
+
+app.get("/mint_to/:userAddress", async (req, res) => {
+  try {
+    const mintAddress = req.params.userAddress;
+    const mintResponse = await mintTo(mintAddress);
+    console.log("mintResponse", mintResponse);
+    res.json({ message: mintResponse });
+  } catch (error) {
+    console.log("error", error);
+    res.json({ message: "error" });
+  }
+});
+
+
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
