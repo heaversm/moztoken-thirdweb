265f4e98e8c6b9a72ff6c4716ff961c14e22613a
Mike Heavers <mheavers@gmail.com>
2022-03-09 14:54:50 -0800

    revert the connect button - not really necessary

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,99 @@
 import React from "react";
 import "./App.css";
-import { ThirdwebProvider } from "@3rdweb/react";
-import Interface from "./Interface";
-function App() {
-  // Put the ethereum chain ids of the chains you want to support
-  const supportedChainIds = [1, 4, 137]; //ethereum(1), ethereum rinkeby (4), polygon(137)
+import moztoken from "./moztoken.jpg"; // gives image path
 
-  const connectors = {
-    injected: {},
+function App() {
+  const [connected, setConnected] = React.useState(null);
+  const [balance, setBalance] = React.useState(null);
+  const [publicKey, setPublicKey] = React.useState("");
+  const [isMinting, setIsMinting] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/connect")
+      .then((res) => res.json())
+      .then((data) => setConnected(data.message));
+  }, []);
+
+  async function getBalance() {
+    fetch(`/get_balance/${publicKey}`)
+      .then((res) => res.json())
+      .then((data) => setBalance(data.balance));
+  }
+
+  async function mintTo() {
+    if (publicKey !== "") {
+      setIsMinting(true);
+      const mintAddress = publicKey;
+      fetch(`/mint_to/${mintAddress}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setIsMinting(false);
+          if (data.message === "success") {
+            getBalance();
+            setError("");
+          } else if (data.message === "error") {
+            setError(
+              "Error minting. It's possible the minter may not have sufficient ETH to pay the gas, or your public key is incorrect"
+            );
+          }
+        });
+    } else {
+      setError("you must enter a public key");
+    }
+  }
+
+  const handlePublicKeyChange = (e) => {
+    setPublicKey(e.target.value);
   };
 
-  /**
-   * Make sure that your app is wrapped with these contexts.
-   * If you're using Next JS, you'll have to replace children with the Component setup
-   */
   return (
-    <ThirdwebProvider
-      connectors={connectors}
-      supportedChainIds={supportedChainIds}
-    >
-      <Interface />
-    </ThirdwebProvider>
+    <div className="App">
+      <header className="App-header">
+        {connected ? (
+          <div className="App-loaded-container">
+            <div className="App-logo-container">
+              <img className="App-logo" src={moztoken} alt="MozToken Logo" />
+            </div>
+            <div className="App-input-container">
+              <label htmlFor="publicKey">Enter your public key:</label>
+              <p>
+                <input
+                  type="text"
+                  className="App-pk-inpu3"
+                  id="publicKey"
+                  value={publicKey}
+                  onChange={handlePublicKeyChange}
+                  placeholder="0x000..."
+                />
+              </p>
+            </div>
+
+            <div className="App-button-container">
+              {!isMinting ? (
+                <button className="App-mint-button" onClick={mintTo}>
+                  Gimme Some MOZ
+                </button>
+              ) : (
+                <button className="App-mint-button">
+                  Minting in progress...
+                </button>
+              )}
+            </div>
+            <div className="App-output-container">
+              {balance && (
+                <p>
+                  <span className="bold">Your Balance:</span> {balance} MOZ
+                </p>
+              )}
+              {error && <p>Error: {error}</p>}
+            </div>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </header>
+    </div>
   );
 }
 
